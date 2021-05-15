f06d95c7f7034973b88a8a4ca6cf3af95b6bd519
Rajat <er.rajat1996@gmail.com>
2021-05-15 18:36:15 +0530

    First commit

---
+++ src/main/java/com/hpf/testmicroservice/TestMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.hpf.testmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hpf/testmicroservice/TestMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hpf.testmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
